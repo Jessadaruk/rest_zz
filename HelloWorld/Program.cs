b06c1241c23685a53b987ecd5121b9e4daef46da
Jessadaruk <jedsadarakwichachai@gmail.com>
2024-11-26 11:12:56 +0700

    add new project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Jessadaruk Wichachai";
+// string is Datatype -> ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Jessadaruk Wichachai");
+Console.WriteLine("My student id 673450207.3");
